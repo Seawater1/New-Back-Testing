20b143edb0dcc030a69053b8c34bf712b0b8e5b1
Seawater1 <57602701+Seawater1@users.noreply.github.com>
2023-04-28 22:01:22 +0100

    notting

--- For loop basic backtester.py
+++ For loop basic backtester.py
@@ -15,8 +15,8 @@ default_parms = {
     'mac': [0],
     'main_or_all': ['all'],
     'filter_by_dates_on': [1],
-    'start_date': ['2023-01-20'], # YYYY-MM-DD Maintickerdatabase starts 2021-04-11 DownloadAll '2021-10-01'
-    'end_date': ['2023-04-26'],
+    'start_date': ['2010-01-20'], # YYYY-MM-DD Maintickerdatabase starts 2021-04-11 DownloadAll '2021-10-01'
+    'end_date': ['2010-12-26'],
     
     # Scanner Settings 
     'volume_min': [-999999],
